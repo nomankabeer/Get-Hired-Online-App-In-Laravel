177594e1aebfea73381d3fef344f8671b283368a
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-05 01:20:34 +0500

    Working on freelancer profile update

---
+++ app/Services/Interfaces/FreelancerUpdateProfile.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noman Kabeer
+ * Date: 03-Dec-2019
+ * Time: 8:05 AM
+ */
+
+namespace App\Services\Interfaces;
+interface FreelancerUpdateProfile
+{
+    public function updateFreelancerProfileImgAndTitle($UserTitleFirstNameLastNameAndProfileImage);
+}
