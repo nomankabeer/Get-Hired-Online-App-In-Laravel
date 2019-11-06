e293f78b9a53e299f9d7a3c69eb866fb80a6948e
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-07 01:22:11 +0500

    Working on order store

---
+++ app/Services/Classes/OrderStoreServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noman Kabeer
+ * Date: 24-Nov-2019
+ * Time: 5:20 AM
+ */
+
+namespace App\Service\Classes;
+use App\Order;
+use App\Services\BaseService;
+class OrderStoreServiceProvider extends BaseService
+{
+    public function createOrderForUser($data){
+        if(Order::create($data)){
+            $status = true;
+            $msg = ['Order Created' , 'Visit order detail page'];
+        }
+        else{
+            $status = false;
+            $msg = ['Something went wrong'];
+        }
+        $data = array(
+            'status' => $status,
+            'msg' => $msg
+        );
+        return $data;
+    }
+}
