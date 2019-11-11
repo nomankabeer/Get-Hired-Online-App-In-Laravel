539cba73db5a79cadb33ffe803289992fb0a54e7
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-12 01:18:19 +0500

    Working on job store service provider

---
+++ app/Services/Classes/JobStoreServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noman Kabeer
+ * Date: 24-Nov-2019
+ * Time: 5:20 AM
+ */
+
+namespace App\Service\Classes;
+use App\Job;
+use Illuminate\Support\Facades\Validator;
+use App\Services\BaseService;
+class JobStoreServiceProvider extends BaseService
+{
+    public function storeClientJob($data){
+        $this->validateStoreData($data)->validate();
+        $data = $this->processDataToStore($data);
+        if(Job::create($data)){
+            $status = true;
+            $msg = ['Job Posted'];
+        }
+        else{
+            $status = false;
+            $msg = ['Something went wrong'];
+        }
+        $data = array(
+            'status' => $status,
+            'msg' => $msg
+        );
+        return $data;
+    }
+    private function validateStoreData($data){
+        return Validator::make($data, [
+            'budget' => ['required', 'integer', 'alpha_dash' ],
+            'title' => ['required', 'string', 'min:10'],
+            'description' => ['required', 'string', 'min:20'],
+        ]);
+    }
+}
