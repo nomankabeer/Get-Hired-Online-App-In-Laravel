76394a2b9c8d637c98f73b8178d8a823d78c1710
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-14 23:37:45 +0500

    Working on freelancer profile

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::view('user/profile' , 'user_profile');
 
 //freelancer routes
 Route::view('freelancer/index' , 'freelancer/index')->name('index');
+Route::view('freelancer/profile' , 'freelancer/index')->name('index');
 //Route::view('job/detail' , 'add_job')->name('job.detail');
 Auth::routes();
 
