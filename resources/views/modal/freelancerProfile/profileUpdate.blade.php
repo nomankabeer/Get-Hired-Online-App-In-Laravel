f2ffb7883b23069d3abda00937fb26cad08fee1d
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-15 04:37:26 +0500

    Working on freelancer profile Modal

---
+++ resources/views/modal/freelancerProfile/profileUpdate.blade.php
@@ -0,0 +1,13 @@
+<div id="update-profile" class="uk-flex-top" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+        <button class="uk-modal-close-default" type="button" uk-close></button>
+       <form method="post" action="?saf">
+           <input name="title" required placeholder="Title" class="uk-input uk-search-input">
+           <div class="js-upload" uk-form-custom>
+               <input type="file" multiple>
+               <button class="uk-button uk-button-primary" type="button" tabindex="-1">Select Image</button>
+           </div>
+           <button type="submit" class="uk-button uk-button-primary">Update Profile</button>
+       </form>
+    </div>
+</div>
