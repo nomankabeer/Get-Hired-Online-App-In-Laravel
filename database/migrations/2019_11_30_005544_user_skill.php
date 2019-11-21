c47c7633a325bc06a8b67820cafe3cc3531a3b7a
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-22 01:12:48 +0500

    Working on migration

--- database/migrations/2019_11_30_005544_user_skill.php
+++ database/migrations/2019_11_30_005544_user_skill.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UserSkill extends Migration
+class FreelancerSkill extends Migration
 {
     /**
      * Run the migrations.
