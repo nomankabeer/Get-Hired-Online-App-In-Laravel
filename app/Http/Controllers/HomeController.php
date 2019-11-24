3716eac0c813e9314d5bba2d937b20319ad582a4
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-25 04:32:29 +0500

    redirect user after login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
+use App\User;
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('account_status');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        if(Auth::user()->role_id == User::freelancerRoleId){
+            return view('frontend.freelancer.index');
+        }
+        elseif(Auth::user()->role_id == User::clientRoleId){
+            return view('frontend.client.index');
+        }
     }
 }
