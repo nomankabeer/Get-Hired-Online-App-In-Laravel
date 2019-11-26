8e9d701bfc7519c0d904f970e6def7022d389e98
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-26 06:37:46 +0500

    dynamic sidebar for freelancer and client

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,26 +5,29 @@
         <!-- Logo -->
         <div id="logo">
             <span class="image avatar48"><img src="{{asset('images/avatar.jpg')}}" alt="" /></span>
-            <h1 id="title">Jane Doe</h1>
+            <h1 id="title">{{Auth::user()->name}}</h1>
             <p>Hyperspace Engineer</p>
         </div>
 
         <!-- Nav -->
         <nav id="nav">
-            //client
-            <ul>
-                <li><a href="{{route('index')}}" id="top-link"><span class="icon fa-home">Intro</span></a></li>
-                {{--<li><a href="{{route('index')}}" id="portfolio-link"><span class="icon fa-th">Portfolio</span></a></li>
-                <li><a href="{{route('index')}}" id="about-link"><span class="icon fa-user">About Me</span></a></li>--}}
-                <li><a href="{{route('post.job')}}" id="contact-link"><span class="icon fa-envelope">Post a Job</span></a></li>
-            </ul>
-
-//freelancer
+            @if(Auth::user()->role_id == $clientRoleId)
+                <ul>
+                    <li><a href="{{route('index')}}" id="top-link"><span class="icon fa-home">Intro</span></a></li>
+                    {{--<li><a href="{{route('index')}}" id="portfolio-link"><span class="icon fa-th">Portfolio</span></a></li>--}}
+                    <li><a href="{{route('index')}}" id="about-link"><span class="icon fa-user">About Me</span></a></li>
+                    <li><a href="{{route('client.job.post')}}" id="contact-link"><span class="icon fa-envelope">Post a Job</span></a></li>
+                    <li><a href="{{route('client.job.list')}}" id="contact-link"><span class="icon fa-envelope">My Job List</span></a></li>
+                    <li><a href="{{route('logout')}}" id="contact-link"><span class="icon fa-envelope">Logout</span></a></li>
+                </ul>
+            @elseif(Auth::user()->role_id == $freelancerRoleId)
             <ul>
                 <li><a href="{{route('index')}}" id="top-link"><span class="icon fa-home">Intro</span></a></li>
                 <li><a href="{{route('index')}}" id="portfolio-link"><span class="icon fa-th">Applied Jobs</span></a></li>
+                <li><a href="{{route('logout')}}" id="portfolio-link"><span class="icon fa-th">Logout</span></a></li>
             </ul>
         </nav>
+        @endif
 
     </div>
 
