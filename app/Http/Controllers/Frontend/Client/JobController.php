dcc40fe7c31c72946150bd8508e117caa7bca006
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-28 03:22:55 +0500

    Working on job module for client to award job to freelancer

--- app/Http/Controllers/Frontend/Client/JobController.php
+++ app/Http/Controllers/Frontend/Client/JobController.php
@@ -5,20 +5,22 @@
  * Date: 24-Nov-2019
  * Time: 5:20 AM
  */
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Frontend\Client;
 use Illuminate\Http\Request;
-use App\Repositories\JobRepository;
+use App\Repositories\Frontend\Client\JobRepository;
+use App\Http\Controllers\Controller;
 class jobController extends Controller
 {
     protected $jobRepository = null;
+    private $jobViews = "frontend.client.job";
     public function __construct(JobRepository $jobRepository){
         $this->jobRepository = $jobRepository;
     }
     public function index(){
-        return view('frontend.client.job_list');
+        return view($this->jobViews.'.job_list');
     }
     public function create(){
-        return view('frontend.client.job_post');
+        return view($this->jobViews.'.job_post');
     }
     public function store(Request $request) {
         return $this->jobRepository->storeJobData($request->all());
@@ -40,5 +42,8 @@ class jobController extends Controller
     }
     public function getJobDetail($id){
         return $this->jobRepository->getUSerJobDetail($id);
+    }
+    public function awardJobBid($job_id , $bid_id){
+        return $this->jobRepository->awardUserBidForJob($job_id , $bid_id);
     }
 }
