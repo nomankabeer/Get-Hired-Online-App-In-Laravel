f93336bba8aa6d656421214a15484ce2d146b42c
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-02 01:45:17 +0500

    Working on order status update module

---
+++ app/Repositories/ServiceProviders/Classes/OrderStatusUpdateServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noman Kabeer
+ * Date: 24-Nov-2019
+ * Time: 5:20 AM
+ */
+
+namespace App\Repositories\ServiceProviders\Classes;
+use App\Order;
+use App\Repositories\ServiceProviders\BaseServiceProvider;
+class OrderStatusUpdateServiceProvider extends BaseServiceProvider
+{
+    /*public function createOrderForUser($data){
+        if(Order::create($data)){
+            $status = true;
+            $msg = ['Order Created' , 'Visit order detail page'];
+        }
+        else{
+            $status = false;
+            $msg = ['Something went wrong'];
+        }
+        $data = array(
+            'status' => $status,
+            'msg' => $msg
+        );
+        return $data;
+    }*/
+}
