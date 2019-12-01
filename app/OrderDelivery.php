f400615877643d2c3cb8c7a81967d1463a3d8942
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-01 20:05:32 +0500

    Working on order delivery module

---
+++ app/OrderDelivery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDelivery extends Model
+{
+    protected $table = "order_delivery";
+
+    public const orderDeliveryStatusPendingId = 1;
+    public const orderDeliveryStatusAcceptedId = 2;
+    public const orderDeliveryStatusRejectedId = 3;
+
+    public const orderDeliveryStatusPending = "Pending";
+    public const orderDeliveryStatusAccepted = "Accepted";
+    public const orderDeliveryStatusRejected = "Rejected";
+
+    protected $fillable = ['order_id' , 'is_accepted' , 'file' , 'created_at' , 'updated_at'];
+
+    protected $appends = ['orderStatus'];
+    public function getorderStatusAttribute(){
+        switch ($this->status){
+            case(self::orderDeliveryStatusPendingId):
+                return self::orderDeliveryStatusPending;
+            case(self::orderDeliveryStatusAcceptedId):
+                return self::orderDeliveryStatusAccepted;
+            case(self::orderDeliveryStatusRejectedId):
+                return self::orderDeliveryStatusRejected;
+        }
+    }
+}
