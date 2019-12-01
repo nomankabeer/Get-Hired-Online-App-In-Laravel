de02950b5ba389887721332e564ee3eb401e94eb
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-01 20:04:59 +0500

    Working on order module

---
+++ app/Order.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = "order";
+
+    public const orderActiveId = 1;
+    public const orderCompletedId = 2;
+    public const orderRejectedId = 3;
+    public const orderDeliveredId = 4;
+    public const orderCanceledId = 5;
+
+    public const orderActive = "Active";
+    public const orderCompleted = "Completed";
+    public const orderRejected = "Rejected";
+    public const orderDelivered = "Delivered";
+    public const orderCanceled = "Canceled";
+
+    protected $fillable = ['bid_id' , 'job_id' , 'status' , 'client_review' , 'freelancer_review' , 'client_rating' , 'freelancer_rating' , 'created_at' , 'updated_at'];
+
+    protected $appends = array('order_status');
+    public function getOrderStatusAttribute(){
+        switch ($this->status){
+            case(self::orderActiveId):
+                $status = self::orderActive;break;
+            case(self::orderDeliveredId):
+                $status = self::orderDelivered;break;
+            case(self::orderRejectedId):
+                $status = self::orderRejected;break;
+            case(self::orderCompletedId):
+                $status = self::orderCompleted;break;
+            case(self::orderCanceledId):
+                $status = self::orderCanceled;break;
+        }
+        return $status;
+    }
+
+    public function orderDeliveries(){
+        return $this->hasMany('App\OrderDelivery' , 'order_id' , 'id');
+    }
+    public function jobDetail(){
+        return $this->hasOne('App\job' , 'id' , 'job_id');
+    }
+    public function bidDetail(){
+        return $this->hasOne('App\Bids' , 'id' , 'bid_id');
+    }
+}
