3e9e6ad97b33c86e7acd270b69dfdb6d63a21ab9
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-01 20:04:28 +0500

    Working on order review module

--- app/Http/Controllers/Frontend/Freelancer/ReviewController.php
+++ app/Http/Controllers/Frontend/Freelancer/ReviewController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Frontend\Order;
 
 use Illuminate\Http\Request;
-
-class BidController extends Controller
+use App\Http\Controllers\Controller;
+class ReviewController extends Controller
 {
     /**
      * Display a listing of the resource.
