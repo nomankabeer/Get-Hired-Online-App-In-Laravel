715871ab669b90e803a8019694ee33f69b9e1c98
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-10 06:58:40 +0500

    Working on freelancer profile

--- app/Repositories/Frontend/Freelancer/FreelancerProfileRepository.php
+++ app/Repositories/Frontend/Freelancer/FreelancerProfileRepository.php
@@ -9,12 +9,10 @@
 namespace App\Repositories\Frontend\Freelancer;
 use App\Repositories\BaseRepository;
 use App\Repositories\ServiceProviders\Classes\FreelancerAddEducation;
+use App\Repositories\ServiceProviders\Classes\FreelancerDeleteEducation;
 use App\Repositories\ServiceProviders\Classes\FreelancerProfileUpdateTitleAndImageServiceProvider;
-use App\User;
-use App\UserDetail;
+use App\Repositories\ServiceProviders\Classes\FreelancerUpdateEducation;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Yajra\DataTables\Facades\DataTables;
 use App\Repositories\Traits\UploadFileTrait;
 use App\Repositories\Traits\FreelancerProfileTrait;
 class FreelancerProfileRepository extends BaseRepository
@@ -22,16 +20,26 @@ class FreelancerProfileRepository extends BaseRepository
     use UploadFileTrait , FreelancerProfileTrait;
     protected $redirect = null;
     protected $addEducation = null;
+    protected $updateEducation = null;
+    protected $deleteEducation = null;
+    protected $updateProfile = null;
     protected $freelancerProfileRoute = 'freelancer.profile';
     protected $freelancerProfileView = 'frontend.freelancer.profile.profile';
 
-    public function __construct(FreelancerAddEducation $addEducation){
+    public function __construct(
+        FreelancerAddEducation $addEducation ,
+        FreelancerProfileUpdateTitleAndImageServiceProvider $updateProfile ,
+        FreelancerUpdateEducation $updateEducation ,
+        FreelancerDeleteEducation $deleteEducation )
+    {
         $this->addEducation = $addEducation;
+        $this->updateProfile = $updateProfile;
+        $this->updateEducation = $updateEducation;
+        $this->deleteEducation = $deleteEducation;
     }
 
     public function updateProfileImgAndTitle($data){
-        $updateProfile = new FreelancerProfileUpdateTitleAndImageServiceProvider();
-        $updated_data = $updateProfile->updateFreelancerProfileImgAndTitle($data , $this->getUserId());
+        $updated_data = $this->updateProfile->updateFreelancerProfileImgAndTitle($data);
         $this->redirect = $this->freelancerProfileRoute;
         return $this->redirectRoute($updated_data['status'] , $updated_data['msg']);
     }
@@ -47,4 +55,17 @@ class FreelancerProfileRepository extends BaseRepository
         $data = $this->addEducation->addEducation($data);
         return $this->redirectRoute($data['status'] , $data['msg']);
     }
+
+    public function updateFreelancerEducation($data){
+        $this->redirect = $this->freelancerProfileRoute;
+        $data = $this->updateEducation->updateEducation($data);
+        return $this->redirectRoute($data['status'] , $data['msg']);
+    }
+
+    public function deleteFreelancerEducation($id){
+        $this->redirect = $this->freelancerProfileRoute;
+        $data = $this->deleteEducation->deleteEducation($id);
+        return $this->redirectRoute($data['status'] , $data['msg']);
+    }
+
 }
