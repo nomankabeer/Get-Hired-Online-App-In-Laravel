a25485d5f8f722ccf76e0a056b2905f0982b47ee
Noman Kabeer <nomankabeerr@gmail.com>
2019-12-14 05:19:13 +0500

    Working on job model

--- app/Job.php
+++ app/Job.php
@@ -9,4 +9,13 @@ class Job extends Model
     use SoftDeletes;
     protected $table = "job";
     protected $fillable = ['title' , 'budget' , 'description' , 'user_id' , 'skills'];
+    protected $appends = array('job_is_awarded');
+    public function getJobIsAwardedAttribute(){
+     $check = Bids::select('is_awarded')->where('job_id' , $this->id)->where('is_awarded' , 1)->first();
+     if($check == null){ $val = 0; } else { $val = 1; }
+     return $val;
+    }
+    public function jobBids(){
+        return $this->hasMany('App\Bids', 'job_id' , 'id');
+    }
 }
