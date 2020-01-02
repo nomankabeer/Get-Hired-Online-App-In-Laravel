a295696323bbc0cf51b1fd42d208eaaa0422f018
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-03 01:58:18 +0500

    Working on upload profile

--- app/Repositories/Traits/UploadFileTrait.php
+++ app/Repositories/Traits/UploadFileTrait.php
@@ -8,6 +8,16 @@ Trait UploadFileTrait {
         $image->move(public_path('/').$path , $name);
         return $name;
     }
+    public function uploadOrderFiles($file , $order_id , $path = 'order_deliveries'){
+        $path = $this->getDateMonthToPathOfOrder($path , $order_id);
+        $name = $path.'/'.time().'.'.$file->getClientOriginalExtension();
+        $file->move(public_path('/').$path , $name);
+        return $name;
+    }
+    private function getDateMonthToPathOfOrder($path , $order_id){
+        $now = Carbon::now();
+        return 'storage/'.$path.'/'.$now->year.'/'.$now->month.'/'.$order_id;
+    }
     private function getDateMonthToPath($path){
         $now = Carbon::now();
         return 'storage/'.$path.'/'.$now->year.'/'.$now->month;
