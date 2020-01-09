da55f1d6157fe93a929ffe3d0a68e6f1ebbb8fe6
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-10 02:04:42 +0500

    Working on order review

--- app/Services/Classes/OrderReview.php
+++ app/Services/Classes/OrderReview.php
@@ -5,16 +5,18 @@
  * Date: 24-Nov-2019
  * Time: 5:20 AM
  */
-namespace App\Service\Classes;
+namespace App\Services\Classes;
 use App\Order;
 use App\Services\BaseService;
 use Illuminate\Support\Facades\Validator;
 class OrderReview extends BaseService
 {
     public function reviewOrder($data){
+
         $this->validateReviewData($data)->validate();
-        $check = $this->currentClientOrder($data['order_id']);
-        if($check === true){
+        $order = Order::where('order_id' , $data['order_id'])->with('jobDetail')->first();
+        $order_detail = $this->currentClientOrder($order);
+        if($order_detail === true){
             $review = $this->storeReview($data);
             if($review){
                 $status = true;
@@ -25,32 +27,31 @@ class OrderReview extends BaseService
                 $msg = ['Something went wrong. Please contact to customer support'];
             }
         }
-        elseif($check === false){
+        elseif($order_detail === false){
             $status = false;
             $msg = ['Order Not Found' , 'if you have any issue please contact to customer support'];
         }
         else{
             $status = false;
-            $msg = ['Error' , $check];
+            $msg = ['Error' , $order_detail];
         }
         $data = array(
             'status' => $status,
             'msg' => $msg,
-            'order_id' => $data['order_id'],
+            'order_id' => $order->id,
         );
         return $data;
     }
 
     private function validateReviewData($data){
         return Validator::make($data, [
-            'order_id' => ['required', 'integer', 'alpha_dash' ],
-            'client_review' => ['required', 'string', 'min:10' , 'regex:/^[a-zA-Z0-9,.!-\s]+$/'],
+            'order_id' => ['required', 'string', 'alpha_dash' ],
+            'client_review' => ['required', 'string', 'min:3' /*, 'regex:/^[a-zA-Z0-9,.!-\s]+$/'*/],
             'client_rating' => ['required', 'integer', 'min:1' , 'max:5'],
         ]);
     }
 
-    private function currentClientOrder($order_id){
-        $order = Order::where('id' , $order_id)->with('jobDetail')->first();
+    private function currentClientOrder($order){
         if($order != null && $order->jobDetail->user_id === $this->getUserId()){
             if($order->status == Order::orderCompletedId){
                 if($order->client_rating == null || $order->client_rating == 0){
@@ -72,7 +73,7 @@ class OrderReview extends BaseService
     private function storeReview($data){
         $order_id = $data['order_id'];
         $data = $this->processDataToStoreRating($data);
-        if(Order::where('id' , $order_id)->update($data)){
+        if(Order::where('order_id' , $order_id)->update($data)){
             return true;
         }
         else{
