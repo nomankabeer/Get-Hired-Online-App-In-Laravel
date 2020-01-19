de46081ce40db4a9ba5bfce746eebe6913e286ea
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-20 02:11:25 +0500

    Working on frontend

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -18,14 +18,18 @@
                     <li><a href="{{route('index')}}" id="about-link"><span class="icon fa-user">About Me</span></a></li>
                     <li><a href="{{route('client.job.post')}}" id="contact-link"><span class="icon fa-envelope">Post a Job</span></a></li>
                     <li><a href="{{route('client.job.list')}}" id="contact-link"><span class="icon fa-envelope">My Job List</span></a></li>
-                    <li><a href="{{route('client.order.list')}}" id="contact-link"><span class="icon fa-envelope">My Order List</span></a></li>
+                    <li><a href="{{route('client.orders.list')}}" id="contact-link"><span class="icon fa-envelope">My Order List</span></a></li>
                     <li><a href="{{route('client.search.freelancer')}}" id="contact-link"><span class="icon fa-envelope">Search Freelancer</span></a></li>
                     <li><a href="{{route('logout')}}" id="contact-link"><span class="icon fa-envelope">Logout</span></a></li>
                 </ul>
             @elseif(Auth::user()->role_id == $freelancerRoleId)
             <ul>
                 <li><a href="{{route('freelancer.profile')}}" id="top-link"><span class="icon fa-home">Profile</span></a></li>
-                <li><a href="{{route('index')}}" id="portfolio-link"><span class="icon fa-th">Applied Jobs</span></a></li>
+                <li><a href="{{route('freelancer.job.view')}}" id="portfolio-link"><span class="icon fa-th">Jobs</span></a></li>
+                <li><a href="{{route('freelancer.applied.job.list.view')}}" id="portfolio-link"><span class="icon fa-th">Applied Jobs</span></a></li>
+                <li><a href="{{route('freelancer.active.order.list.view')}}" id="portfolio-link"><span class="icon fa-th">Active Orders</span></a></li>
+                <li><a href="{{route('freelancer.completed.job.list.view')}}" id="portfolio-link"><span class="icon fa-th">completed Jobs</span></a></li>
+                <li><a href="{{route('freelancer.cancelled.job.list.view')}}" id="portfolio-link"><span class="icon fa-th">Cancelled Jobs</span></a></li>
                 <li><a href="{{route('logout')}}" id="portfolio-link"><span class="icon fa-th">Logout</span></a></li>
             </ul>
         </nav>
