574bda98ced36f857a997942218c014a65988597
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-21 02:12:11 +0500

    Working on frontend

--- resources/views/modal/freelancerProfile/skills.blade.php
+++ resources/views/modal/freelancerProfile/skills.blade.php
@@ -1,9 +1,15 @@
 <div id="skills" class="uk-flex-top" uk-modal>
     <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
         <button class="uk-modal-close-default" type="button" uk-close></button>
-       <form method="post" action="?saf">
-           <input name="title" required placeholder="Skill Name" class="uk-input uk-search-input">
-           <button type="submit" class="uk-button uk-button-primary">Update Profile</button>
+       <form method="post" action="{{route('freelancer.add.skill')}}">
+           @csrf
+           <select name="skill_id" required>
+               <option disabled selected>Select</option>
+               @foreach($data['skills'] as $skill)
+               <option value="{{$skill->id}}" >{{$skill->name}}</option>
+               @endforeach
+           </select>
+           <button type="submit" class="uk-button uk-button-primary">Add Skill</button>
        </form>
     </div>
 </div>
