67d930e5e9cc1a8e5988381396c45aacc6de32b2
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-23 02:12:40 +0500

    Working on frontend

--- resources/views/modal/freelancerProfile/experience.blade.php
+++ resources/views/modal/freelancerProfile/experience.blade.php
@@ -1,10 +1,11 @@
 <div id="experience" class="uk-flex-top" uk-modal>
     <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
         <button class="uk-modal-close-default" type="button" uk-close></button>
-        <form method="post" action="?saf">
+        <form method="post" action="{{route('freelancer.add.experience')}}">
+            @csrf
             <input name="title" required placeholder="Title" class="uk-input uk-search-input">
-            <input name="start" required placeholder="Start Date" class="uk-input uk-search-input">
-            <input name="end" required placeholder="End Date" class="uk-input uk-search-input">
+            <input type="date" name="start_date" required placeholder="Start Date" class="uk-input uk-search-input">
+            <input type="date" name="end_date" required placeholder="End Date" class="uk-input uk-search-input">
             <textarea name="description" required placeholder="Description" class="uk-input uk-search-input"></textarea>
 
             <button type="submit" class="uk-button uk-button-primary">Add Experience</button>
