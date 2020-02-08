eb3e824d96094b2701a22cc756512f6cacb70934
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-02-09 03:05:32 +0500

    Working on frontend

---
+++ resources/views/frontend/freelancer/cancelled_job/job_list.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+@section('content')
+ <table class="table table-bordered" id="job-table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Budget</th>
+            <th>Title</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+    </table>
+@endsection
+@push('script')
+    <script>
+        $(function() {
+            $('#job-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{!! route('freelancer.cancelled.job.list') !!}',
+                columns: [
+                    { data: 'id', name: 'id' },
+                    { data: 'budget', name: 'budget' },
+                    { data: 'title', name: 'title' },
+                    { data: 'created_at', name: 'created_at' },
+                    { data: 'updated_at', name: 'updated_at' },
+                    { data: 'action', name: 'action' }
+                ]
+            });
+        });
+    </script>
+@endpush
